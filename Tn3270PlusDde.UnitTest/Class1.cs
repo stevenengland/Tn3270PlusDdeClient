49ae11a0bc50182d5539660b7c31f75609208f2d
England <S.England@telekom.de>
2019-02-26 10:45:59 +0100

    added StyleCop and XUnit

--- Tn3270PlusDde.UnitTest/Class1.cs
+++ Tn3270PlusDde.UnitTest/Class1.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Tn3270PlusDde.UnitTest
 {
     public class Class1
     {
+        [Fact]
+        public void Test()
+        {
+
+        }
     }
 }
